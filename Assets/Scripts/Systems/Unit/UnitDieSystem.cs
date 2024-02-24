fd1acb63e308044c2597805e8b033fc43481b714
Мусо Исмаилов <musoismailov16@gmail.com>
2024-02-25 00:57:56 +0500

    Scripts moved and some fixes

--- Assets/Scripts/Systems/Unit/UnitDieSystem.cs
+++ Assets/Scripts/Systems/Unit/UnitDieSystem.cs
@@ -25,7 +25,6 @@ namespace Inventory.Systems
                     continue;
                 }
                 
-                Debug.Log("Die");
                 var unitDieEvent = new UnitDieEvent {View = unit.View};
                 _eventWorld.Value.SendEvent(unitDieEvent);
             }
